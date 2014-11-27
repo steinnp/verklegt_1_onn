a087b7cbe32a56b6bb505ff61b22d3bc07bd6560
steinnp <steiniell@gmail.com>
2014-11-27 16:06:02 +0000

    stafsetning

--- Man.cpp
+++ Man.cpp
@@ -30,7 +30,7 @@ ofstream& operator << (ofstream& os, Man& m1)
 ifstream& operator >> (ifstream& is, Man& m1)
 {
     cout << "Ifstream" << endl;
-    cout << "First Name: ";
+    cout << "First Nam: ";
     is >> m1.firstName;
     cout << "Last Name: ";
     is >> m1.lastName;
